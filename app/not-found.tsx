241c05a481247374e45c374e9c00e425b8a8da62
imrany <imranmat254@gmail.com>
2023-08-13 18:15:56 +0300

    added a 404 page

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,8 +1,16 @@
+import Link from "next/link";
+import type { Metadata } from 'next'
 
+export const metadata: Metadata = {
+  title: 'Page Not Found',
+  description: 'This page does not exist',
+}
 export default function NotFound() {
   return (
-    <main className="flex h-[100vh] justify-center items-center">
+    <main className="flex h-[100vh] flex-col justify-center items-center">
+      <i className="ri-folder-warning-fill ri-3x"></i>
       <p className="text-xl">This page does not exist</p>
+      <Link href="/" className="bg-black text-white w-[100px] h-[35px] rounded-[50px] flex justify-center items-center mt-6">Back</Link>
     </main>
   )
 }
