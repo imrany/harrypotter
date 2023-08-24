1c62184f4ffcae2ee9bf4ca53be863e5adf245aa
imrany <imranmat254@gmail.com>
2023-08-24 12:49:14 +0300

    trail.js

---
+++ app/trail.js
@@ -0,0 +1,31 @@
+const  person={
+    id:0,
+    name:["firtsname","lastname"],
+    age:18,
+    weight:56.3 
+} 
+// delete person.age
+// const {age, ...new_person}=person
+// console.log(new_person)
+
+// const firstname=()=>{
+//     return "imran"
+// }
+// function firstname(){
+//     return "imran"
+// }
+// console.log(firstname())
+// var updatedObject = Object.keys(person).reduce(
+//     (acc, key)=>{
+//        if(key != "age"){
+//          acc[key] = person[key];
+//        }
+//     return acc;
+// },{});
+  
+// console.log(updatedObject);
+
+function add (x,y){
+    return x+y;
+}
+
